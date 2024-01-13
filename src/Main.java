2e31316b8e8f46ce07fc807f260cde554100a112
ElenaN <maggika@mail.ru>
2024-01-13 23:12:56 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+// 1
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
 
+// 2
+        String fullNameUpperCase = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameUpperCase);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 3
+        String fullName3 = "Иванов Семён Семёнович";
+        String fullNameNew = fullName3.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameNew);
     }
 }
